257cc6eec7f301becfc50b43b9e05ca2a90eacca
ivanbondarenko <ivan333m@gmail.com>
2014-01-15 19:50:34 +0200

    #MS0-14 Init commit: maven project config

---
+++ src/web-tests/src/test/java/com/baremetalstudios/elcadlib/selenium/SmokeTest.java
@@ -0,0 +1,17 @@
+// ============================================================================
+//  File          : SmokeTest
+//  Created       : 14.01.2014   
+//  Description   :
+//  Modifications :
+//
+// ============================================================================
+//  Copyright(c) 2014 Bare Metal Studios, Ukraine
+// ============================================================================
+package com.baremetalstudios.elcadlib.selenium;
+
+/**
+ * @author Ivan Bondarenko
+ * @version 1.0
+ */
+public class SmokeTest {
+}
---
+++ src/web/src/main/java/com/baremetalstudios/elcadlib/web/controller/TestController.java
@@ -0,0 +1,17 @@
+// ============================================================================
+//  File          : TestController
+//  Created       : 14.01.2014   
+//  Description   :
+//  Modifications :
+//
+// ============================================================================
+//  Copyright(c) 2014 Bare Metal Studios, Ukraine
+// ============================================================================
+package com.baremetalstudios.elcadlib.web.controller;
+
+/**
+ * @author Ivan Bondarenko
+ * @version 1.0
+ */
+public class TestController {
+}
